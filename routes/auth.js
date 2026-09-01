1b9bcdbb032ab50b33ebffb2c97ae4e0c10943c9
Pedro Nahun Suazo Cole <102774800+pedrosuazocole@users.noreply.github.com>
2026-09-01 14:15:16 -0600

    Fase 1

--- routes/auth.js
+++ routes/auth.js
@@ -6,10 +6,11 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const { redirectIfAuthenticated, isAuthenticated } = require('../middleware/auth');
+const { limitadorLogin } = require('../middleware/rateLimit');
 
 // Login
 router.get('/login', redirectIfAuthenticated, authController.mostrarLogin);
-router.post('/login', redirectIfAuthenticated, authController.procesarLogin);
+router.post('/login', limitadorLogin, redirectIfAuthenticated, authController.procesarLogin);
 
 // Logout
 router.get('/logout', authController.logout);
